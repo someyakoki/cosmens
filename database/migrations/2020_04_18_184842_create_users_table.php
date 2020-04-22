50b70d0834c162dc79b4c9fa7abe4f12e451bab1
someyakoki <yukiteru274@icloud.com>
2020-04-22 18:34:57 +0900

    push

---
+++ database/migrations/2020_04_18_184842_create_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('birthday');
+            $table->string('skin_type');
+            $table->string('skin_trouble');
+            $table->dateTime('created_at');
+            $table->dateTime('modified');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
